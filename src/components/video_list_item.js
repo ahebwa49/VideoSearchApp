d8dcf0125e3838a7c333f66172bb6c49958aeca7
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-24 16:10:42 +0300

    Add markup for the video-list item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const VideoListItem = ({video}) =>{
   return(
+    <li>
+    <div className ="video-list media" className = "list-group-item">
+        <div className ="media-left">
+          <img className = "media-object"/>
+        </div>
 
+        <div className = "media-body">
+          <div className = "media-heading"></div>
+        </div>
+      </div>
+    </li>
   )
 }
 
