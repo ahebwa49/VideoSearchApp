c68edf09561e3e949e9f2c81d9942fc454f3d620
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-24 14:46:49 +0300

    Import VideoList Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCAfItybV7pSzxmwTj5wyG5SErMs5HYlRQ';
 
@@ -18,6 +19,7 @@ class App extends Component(){
   render(){
     return(
           <SearchBar />
+
     );
   }
 };
