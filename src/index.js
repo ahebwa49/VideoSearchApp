6e7bb03d3a0a0474eed5b958e69566b4369ebad0
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-24 12:46:06 +0300

    Store the API key

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
+const API_KEY = 'AIzaSyCAfItybV7pSzxmwTj5wyG5SErMs5HYlRQ';
+
 
 class App extends Component(){
   render(){
