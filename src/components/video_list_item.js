3ffb347bc133ee1067fd8d0e44a5049d4699eb94
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-24 16:48:38 +0300

    Reference the image url

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -8,7 +8,7 @@ const imgUrl = video.snippet.thumbnails.default.url;
     <li className = "list-group-item">
     <div className ="video-list media" >
         <div className ="media-left">
-          <img className = "media-object"/>
+          <img className = "media-object" src = {imgUrl}/>
         </div>
 
         <div className = "media-body">
