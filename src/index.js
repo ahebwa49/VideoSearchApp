2a2893f51e62dff81e44afb901ddda6e3434cf39
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-24 12:43:49 +0300

    Import YTSearch from the library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
 
 class App extends Component(){
   render(){
