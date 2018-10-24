4454550306d38e9944a0910bd4dc91ec99d50996
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-24 18:08:42 +0300

    return an array of the videos as videoItems.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,11 +2,13 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  props.videos.map((video) => {
+  const videoItems = props.videos.map((video) => {
     return <VideoListItem video = {video}/>;
   });
   return(
-
+    <ul>
+      {videoItems}
+    </ul>
   );
 }
 
