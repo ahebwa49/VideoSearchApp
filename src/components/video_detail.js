bba41b178aaacaab6268539a42d82a86141db8c0
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-25 05:07:49 +0300

    Add a reference to the video description

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -12,7 +12,7 @@ const VideoDetail = ({video}) =>{
       </div>
       <div className="details">
       <div>{video.snippet.title}</div>
-      <div></div>
+      <div>{video.snippet.description}</div>
       </div>
     </div>
   )
