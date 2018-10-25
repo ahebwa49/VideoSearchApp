5f1135b0f5b6136e7d2841349d1abaeeb8d757da
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-25 04:46:38 +0300

    Return markup for the VideoDetail component

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const VideoDetail = () =>{
-  return(){
-
-  }
+  return(
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item"></iframe>
+      </div>
+      <div className="details">
+      <div></div>
+      <div></div>
+      </div>
+    </div>
+  )
 }
 
 export default VideoDetail;
