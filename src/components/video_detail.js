9055e1a8467048bf43cb33975f1277cc135af2b7
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-25 05:21:59 +0300

    Check if video has not yet been fetched and display loading...

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const VideoDetail = ({video}) =>{
+  if (!video){
+    return <div>Loading...</div>;
+  }
 
   const videoId = video.id.videoId;
-  const url = `www.youtube.com/embed/${videoId}`;
+  const url = `https://www.youtube.com/embed/${videoId}`;
 
   return(
     <div className="video-detail col-md-8">
@@ -15,7 +18,7 @@ const VideoDetail = ({video}) =>{
       <div>{video.snippet.description}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default VideoDetail;
