5ea3bb4b301315fd5728d507544155e8bf58f5bb
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-25 04:56:48 +0300

    Retrieve the videoId from the video object

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const VideoDetail = ({video}) =>{
+
+  const videoId = video.id.videoId;
+
+
   return(
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
