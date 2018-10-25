9fd23adf6d0f39bdf7fc74186c4ddd0ea3ad024c
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-25 05:10:10 +0300

    Import VideoDetail component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCAfItybV7pSzxmwTj5wyG5SErMs5HYlRQ';
 
