3de701bb38595f3177ad1a18accca63bd5dbf499
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-26 22:52:21 +0300

    import lodash from the library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 
 const API_KEY = 'AIzaSyCAfItybV7pSzxmwTj5wyG5SErMs5HYlRQ';
 
