8b92921796ad8b62997e75367bfdaa654d66bc4a
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-26 21:01:12 +0300

    call the videoSearch method inside the constructor

--- src/index.js
+++ src/index.js
@@ -15,16 +15,16 @@ class App extends Component{
       videos: [],
       selectedVideo: null
       };
+    this.videoSearch('internet of things');
+      }
 
-
-      });
-    });
-  }
   videoSearch(term){
     YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
+      });
+    });
   }
   render() {
     return (
