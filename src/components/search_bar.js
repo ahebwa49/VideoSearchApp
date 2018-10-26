6f656efc3f854f56106e7fa7811a3dd0f7d141b4
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-26 21:58:00 +0300

    Call the call-back method with the search term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,13 +8,14 @@ class SearchBar extends Component{
   }
   render(){
     return(
-      <div>
+      <div className="search-bar">
       <input onChange = {(event) => this.onInputChange(event.target.value)} />
       </div>
     );
   }
-  onInputChange(event){
+  onInputChange(term){
     this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 };
 
